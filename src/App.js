a1033f99eae5c1c21d1a69193a9a382b44492a22
Farah Fairuz <farahfairuz014@gmail.com>
2025-01-24 14:44:13 +0700

    feat: setup github pages deployment

--- src/App.js
+++ src/App.js
@@ -25,6 +25,37 @@
 
 // export default App;
 
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Resume from "./pages/AboutMe";
+// import Projects from "./pages/Projects";
+// import Experience from "./pages/Experience";
+// import NavBar from "./components/NavBar";
+// import Footer from "./components/Footer";
+
+// function App() {
+//   const basename = process.env.NODE_ENV === "production" ? "/porto-farah" : "/";
+
+//   return (
+//     <Router basename={basename}>
+//       <NavBar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/resume" element={<Resume />} />
+//         <Route path="/projects" element={<Projects />} />
+//         <Route path="/experience" element={<Experience />} />
+//         <Route path="*" element={<Home />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Resume from "./pages/AboutMe";
@@ -34,10 +65,8 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function App() {
-  const basename = process.env.NODE_ENV === "production" ? "/porto-farah" : "/";
-
   return (
-    <Router basename={basename}>
+    <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
